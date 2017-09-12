61e44e88308d01eddffb27adb6b8c43e07dc7acf
tylermiller94 <tylermiller94@gmail.com>
2017-09-12 05:08:31 -0700

    pass test and string is organized alphabeticaly

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,6 @@
 class String
   define_method(:anagrams) do
     letters_array = self.split("")
-    letters_array
+    letters_array.sort
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,8 +2,8 @@ require('rspec')
 require('anagrams')
 
 describe('anagrams') do
-    it(" splits a string into an array with each letter") do
-    expect(("bury").anagrams()).to(eq(["b","u","r","y"]))
+ it(" splits a string into an array with each letter and sorts it alaphabetically") do
+ expect(("race").anagrams()).to(eq(["a","c","e","r"]))
 
   end
 end
