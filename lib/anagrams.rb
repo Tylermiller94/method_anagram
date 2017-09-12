f88343ab4a703f4163fd828b1f3300b269b9099e
tylermiller94 <tylermiller94@gmail.com>
2017-09-12 05:39:05 -0700

    update spec test and anagram.rb

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,10 @@
 class String
-  define_method(:anagrams) do
-    letters_array = self.downcase.split("")
-    sorted_letters = letters_array.sort
-    sorted_letters
+  define_method(:anagrams?) do |second|
+
+   letters_array = self.downcase.split("")
+   sorted_letters = letters_array.sort
+   second_array = second.downcase.split("")
+   sorted_second = second_array.sort
+   sorted_letters == sorted_second
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,14 +1,10 @@
 require('rspec')
 require('anagrams')
 
+
 describe('anagrams') do
- it(" splits a string into an array with each letter and sorts it alphabetically") do
- expect(("race").anagrams()).to(eq(["a","c","e","r"]))
 
-end
-
- it(" splits a string into an array with each letter, downcases it and sorts it alphabetically") do
- expect(("RacE").anagrams()).to(eq(["a","c","e","r"]))
-
+  it("checks it two strings are both equal to the same downcased alaphabetized string") do
+    expect('race'.anagrams?('care')).to(eq(true))
   end
 end
