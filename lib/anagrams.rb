a32d2b8febcd1cba7c35854d2547a51f808cfcd6
tylermiller94 <tylermiller94@gmail.com>
2017-09-12 05:18:56 -0700

    add second test for downcasing as well as code to downcase input

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,6 +1,7 @@
 class String
   define_method(:anagrams) do
-    letters_array = self.split("")
-    letters_array.sort
+    letters_array = self.downcase.split("")
+    sorted_letters = letters_array.sort
+    sorted_letters
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,8 +2,13 @@ require('rspec')
 require('anagrams')
 
 describe('anagrams') do
- it(" splits a string into an array with each letter and sorts it alaphabetically") do
+ it(" splits a string into an array with each letter and sorts it alphabetically") do
  expect(("race").anagrams()).to(eq(["a","c","e","r"]))
+
+end
+
+ it(" splits a string into an array with each letter, downcases it and sorts it alphabetically") do
+ expect(("RacE").anagrams()).to(eq(["a","c","e","r"]))
 
   end
 end
