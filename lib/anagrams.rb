0590fd8e47bb1180f38a240d8775bc5ad291d8a5
tylermiller94 <tylermiller94@gmail.com>
2017-09-12 05:58:22 -0700

    add third test to check for vowells, passed

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,10 +1,28 @@
 class String
-  define_method(:anagrams?) do |second|
 
+  define_method(:word?) do
+    vowell_count = self.scan(/[aeiouy]/).count
+    if vowell_count == 0
+      "input real words"
+    end
+    end
+    define_method(:anagrams?) do |second|
    letters_array = self.downcase.split("")
    sorted_letters = letters_array.sort
    second_array = second.downcase.split("")
    sorted_second = second_array.sort
-   sorted_letters == sorted_second
+   if ((sorted_letters) == (sorted_second))
+   "These words are anagrams."
   end
+end
+
+define_method(:palindromes?) do |second|
+    letters_array = self.downcase.split("")
+    second_array = second.downcase.split("")
+    sorted_second = second_array.sort
+    second_array_reversed = second_array.reverse
+    if((letters_array) == (second_array_reversed))
+      "These words are palindromes."
+  end
+end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,7 +4,13 @@ require('anagrams')
 
 describe('anagrams') do
 
-  it("checks it two strings are both equal to the same downcased alaphabetized string") do
-    expect('race'.anagrams?('care')).to(eq(true))
+  it("checks if the string is an actual word") do
+    expect('rrr'.word?()).to(eq("input real words"))
+  end
+  it("checks if two strings are the same downcased alaphabetized string") do
+    expect('race'.anagrams?('care')).to(eq("These words are anagrams."))
+  end
+  it("checks if two strings are palindromes") do
+  expect('racecar'.palindromes?('racecar')).to(eq("These words are palindromes."))
   end
 end
