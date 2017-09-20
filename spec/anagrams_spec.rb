ad38383c37e61232bb1ba8d829f54260f91eae2d
tylermiller94 <tylermiller94@gmail.com>
2017-09-20 03:53:10 -0700

    old project for later rework

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,39 +1,19 @@
-require('anagrams')
 require('rspec')
+require('anagrams')
 
 
-describe(Anagrams)
+describe('anagrams') do
 
-  describe('#is_anagram?')do
-    it("check input for word")do
-      test_anagram = Anagrams.new("rat","tar")
-      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams"))
-    end
-    it("check for vowels")do
-      test_anagram = Anagrams.new("rat","tar")
-      expect(test_anagram.is_anagram?()).to eq("These words are anagrams")
-    end
-    it("check input for any words that have capitals")do
-      test_anagram = Anagrams.new("Rat","Tar")
-      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams"))
-    end
-    it("checking input for antigrams.")do
-      test_anagram = Anagrams.new("goodbye","air")
-      expect(test_anagram.is_anagram?()).to(eq("These words are not anagrams, they are antigram"))
-    end
-
-  describe('#is_palindrome?')do
-    it("check input for palindrome")do
-      test_anagram = Anagrams.new("dad", "dad")
-      expect(test_anagram.is_palindrome?()).to(eq("this is a palindrome"))
-    end
+  it("checks if the string is an actual word") do
+    expect('rrr'.word?()).to(eq("input real words"))
   end
-
-  describe('#(spechar?')do
-    it("check input for spaces and special charachters and remove them")do
-      test_anagram = Anagrams.new(" rats/*","rat%$*s")
-      expect(test_anagram.spechar?()).to(eq("rats"))
-    end
+  it("checks if two strings are the same downcased alaphabetized string") do
+    expect('race'.anagrams?('care')).to(eq("These words are anagrams."))
   end
-
+  it("checks if two strings are palindromes") do
+    expect('racecar'.palindromes?('racecar')).to(eq("These words are palindromes."))
+  end
+  it("removes non-numbers from the string") do
+    expect('1ri'.anagrams?('ir')).to(eq("These words are anagrams."))
+  end
 end
