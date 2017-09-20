722a7eedf7ffad91ab25a161fed659462523ec98
tylermiller94 <tylermiller94@gmail.com>
2017-09-20 03:45:27 -0700

    add instances and tests checking for palindromes and antigrams, 6 tests pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,27 +1,36 @@
-class String
 
-define_method(:word?) do
-  vowell_count = self.scan(/[aeiouy]/).count
-  if vowell_count == 0
-  "input real words"
-  end
+class Anagrams
+
+attr_accessor(:word1, :word2)
+
+def initialize(word1, word2)
+  @word1 = word1
+  @word2 = word2
 end
-  define_method(:anagrams?) do |second|
-   letters_array = self.gsub(/[^a-zA-Z]/, '').downcase.split("")
-   sorted_letters = letters_array.sort
-   second_array = second.gsub(/[^a-zA-Z]/, '').downcase.split("")
-   sorted_second = second_array.sort
-   if ((sorted_letters) == (sorted_second))
-   "These words are anagrams."
+
+def is_anagram?
+  if @word1.include?("/[aeoui]/") || @word2.include?("a") || @word2.include?("e") || @word2.include?("i") || @word2.include?("o") || @word2.include?("u")
+    if @word1.downcase.split('').sort == @word2.downcase.split('').sort
+      return "These words are anagrams"
+    else
+        return "These words are not anagrams, they are antigram"
+      end
+    else
+      return "This is not a real word. Enter a word with a vowel to make it valid"
+    end
   end
-end
-  define_method(:palindromes?) do |second|
-    letters_array = self.downcase.split("")
-    second_array = second.downcase.split("")
-    sorted_second = second_array.sort
-    second_array_reversed = second_array.reverse
-    if((letters_array) == (second_array_reversed))
-      "These words are palindromes."
+
+  def spechar?
+    @word1.gsub!(/[^abcdefghijklmnopqrstuvwxyz ]/,'')
+    @word2.gsub!(/[^abcdefghijklmnopqrstuvwxyz ]/,'')
+  end
+
+
+  def is_palindrome?
+    if @word1 == @word1.reverse && @word2 == @word2.reverse && @word1 == @word2
+      return "this is a palindrome"
+    else
+      return "This is not a palindrome"
     end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,19 +1,39 @@
-require('rspec')
 require('anagrams')
+require('rspec')
 
 
-describe('anagrams') do
+describe(Anagrams)
 
-  it("checks if the string is an actual word") do
-    expect('rrr'.word?()).to(eq("input real words"))
+  describe('#is_anagram?')do
+    it("check input for word")do
+      test_anagram = Anagrams.new("rat","tar")
+      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams"))
+    end
+    it("check for vowels")do
+      test_anagram = Anagrams.new("rat","tar")
+      expect(test_anagram.is_anagram?()).to eq("These words are anagrams")
+    end
+    it("check input for any words that have capitals")do
+      test_anagram = Anagrams.new("Rat","Tar")
+      expect(test_anagram.is_anagram?()).to(eq("These words are anagrams"))
+    end
+    it("checking input for antigrams.")do
+      test_anagram = Anagrams.new("goodbye","air")
+      expect(test_anagram.is_anagram?()).to(eq("These words are not anagrams, they are antigram"))
+    end
+
+  describe('#is_palindrome?')do
+    it("check input for palindrome")do
+      test_anagram = Anagrams.new("dad", "dad")
+      expect(test_anagram.is_palindrome?()).to(eq("this is a palindrome"))
+    end
   end
-  it("checks if two strings are the same downcased alaphabetized string") do
-    expect('race'.anagrams?('care')).to(eq("These words are anagrams."))
+
+  describe('#(spechar?')do
+    it("check input for spaces and special charachters and remove them")do
+      test_anagram = Anagrams.new(" rats/*","rat%$*s")
+      expect(test_anagram.spechar?()).to(eq("rats"))
+    end
   end
-  it("checks if two strings are palindromes") do
-    expect('racecar'.palindromes?('racecar')).to(eq("These words are palindromes."))
-  end
-  it("removes non numbers from the string") do
-    expect('1ri'.anagrams?('ri')).to(eq("These words are anagrams."))
-  end
+
 end
